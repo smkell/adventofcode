e10ee610ce9567cb8c1db14825bc6d3c185f8913
Sean Kell <sean.michael.kell@gmail.com>
2015-12-08 16:55:15 -0500

    Mostly working.
    There's an infinite cylce in run_program though.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -56,10 +56,153 @@
 /// In little Bobby's kit's instructions booklet (provided as your puzzle input), what signal is 
 /// ultimately provided to _wire_ `a`?
 
-extern crate regex;
-
 mod parser;
 
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+	use std::fs::File;
+	use std::io::prelude::*;
+
+	let mut f = File::open("input.txt").unwrap();
+	let mut buffer = String::new();
+
+	f.read_to_string(&mut buffer).unwrap();
+
+	let registers = run_program(&buffer);
+	println!("The value in register 'a' after running the program is {:?}", registers.get(&"a").unwrap());
+}
+
+/// Runs a program.
+///
+/// # Notes
+/// We need to implement dependncy resolution such that we verify that instructions are executed in 
+/// the correct order. We can do this by sorting by the destination of each instruction.
+///
+/// Instead of iterating over the instructions we should pop the top instruction off the stack. If 
+/// it's a Load instruction then execute it. If it's any other instruction then check to see if the 
+/// neccesary registers are initialized, if they are not then push the instruction back on the stack,
+/// and so on until all instructions have been resolved.
+fn run_program(input: &str) -> HashMap<&str, u16> {
+	use parser::{Instruction, parse_input};
+	let mut instructions = parse_input(input);
+	instructions.reverse();
+
+	let mut registers = HashMap::new();
+
+	while instructions.len() > 0 {
+		let instruction = instructions.pop().unwrap();
+
+		println!("Processing instruction {:?}", instruction);
+		match instruction {
+			Instruction::Load(d, c) => {
+				registers.insert(d, c);
+			},
+			Instruction::And(d, x, y) => {
+				// Check to see if we've initialized the neccesary 
+				// registers
+				if registers.contains_key(&x) && registers.contains_key(&y) {
+					// OK! we can run the instruction
+					let val_x = *registers.get(&x).unwrap();
+					let val_y = *registers.get(&y).unwrap();
+
+					registers.insert(d, val_x & val_y);
+				} else {
+					instructions.insert(0, instruction);
+				}
+			},
+			Instruction::Or(d, x, y) => {
+				// Check to see if we've initialized the neccesary 
+				// registers
+				if registers.contains_key(&x) && registers.contains_key(&y) {
+					// OK! we can run the instruction
+					let val_x = *registers.get(&x).unwrap();
+					let val_y = *registers.get(&y).unwrap();
+
+					registers.insert(d, val_x | val_y);
+				} else {
+					instructions.insert(0, instruction);
+				}
+			},
+			Instruction::LShift(d, x, c) => {
+				// Check to see if we've initialized the neccesary 
+				// registers
+				if registers.contains_key(&x) {
+					// OK! we can run the instruction
+					let val_x = *registers.get(&x).unwrap();
+
+					registers.insert(d, val_x << c);
+				} else {
+					instructions.insert(0, instruction);
+				}
+			},
+			Instruction::RShift(d, x, c) => {
+				// Check to see if we've initialized the neccesary 
+				// registers
+				if registers.contains_key(&x) {
+					// OK! we can run the instruction
+					let val_x = *registers.get(&x).unwrap();
+
+					registers.insert(d, val_x >> c);
+				} else {
+					instructions.insert(0, instruction);
+				}
+			},
+			Instruction::Not(d, x) => {
+				// Check to see if we've initialized the neccesary 
+				// registers
+				if registers.contains_key(&x) {
+					// OK! we can run the instruction
+					let val_x = *registers.get(&x).unwrap();
+
+					registers.insert(d, !val_x);
+				} else {
+					instructions.insert(0, instruction);
+				}
+			},
+		}
+		println!("Register state {:?}", registers);
+	}
+
+	registers
+}
+
+#[test]
+fn run_program_test() {
+	let program = 
+		r"123 -> x
+			456 -> y
+			x AND y -> d
+			x OR y -> e
+			x LSHIFT 2 -> f
+			y RSHIFT 2 -> g
+			NOT x -> h
+			NOT y -> i";
+	let mut expect = HashMap::new();
+	expect.insert("d", 72);
+	expect.insert("e", 507);
+	expect.insert("f", 492);
+	expect.insert("g", 114);
+	expect.insert("h", 65412);
+	expect.insert("i", 65079);
+	expect.insert("x", 123);
+	expect.insert("y", 456);
+
+	println!("Running program {}", program);
+
+	assert_eq!(expect, run_program(program));
+
+	let program = 
+		r"b RSHIFT 5 -> f
+		e AND f -> h
+		b RSHIFT 3 -> e
+		44430 -> b";
+	let mut expect = HashMap::new();
+	expect.insert("b", 44430);
+	expect.insert("e", 44430 >> 3);
+	expect.insert("f", 44430 >> 5);
+	expect.insert("h", (44430 >> 3) & (44430 >> 5));
+
+	println!("Running program {}", program);
+	assert_eq!(expect, run_program(program));
 }
--- day7/src/parser.rs
+++ day7/src/parser.rs
@@ -1,100 +1,272 @@
 /// Provides a parser for the Advent Assembly Language.
 
+/// A Token represents a gramattical token in a program.
+/// 
+/// # Lifetimes
+/// 
+/// * `'a` - Represents the lifetime of the Token object.
 #[derive(Copy, Clone, Debug, PartialEq)]
-enum Instruction {
-	And(char, char, char),
-	Load(char, u32)
+enum Token<'a> {
+	Assign,
+	And,
+	Constant(u16),
+	LShift,
+	Not,
+	Or,
+	RShift,
+	Wire(&'a str),
 }
 
-fn parse_input(input: &str) -> Vec<Instruction> {
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Instruction<'a> {
+	And(&'a str, &'a str, &'a str),
+	Load(&'a str, u16),
+	LShift(&'a str, &'a str, u16),
+	Not(&'a str, &'a str),
+	Or(&'a str, &'a str, &'a str),
+	RShift(&'a str, &'a str, u16),
+}
+
+pub fn parse_input(input: &str) -> Vec<Instruction> {
 	let tokens = tokenize_input(input);
 
-	let mut instructions: Vec<Instruction> = Vec::new();
+	let mut instructions = Vec::new();
 
-	for i in 0..tokens.len() {
-		let token = tokens[i];
-
-		match token {
-			Token::Constant(c) => {
-				// Make sure there's enough tokens available
-				if i < tokens.len() - 2 {
-					match tokens[i+1] {
-						Token::Assign => {
-							match tokens[i+2] {
-								Token::Wire(w) => {
-									instructions.push(Instruction::Load(w, c));
-								}
-								_ => println!("Expression not currently implemented")
-							}
-						},
-						_ => println!("Expression not currently implemented")
-					}
-				}
-			}
-			_ => println!("Expression not currently implemented")
+	let mut start = 0;
+	while start < tokens.len() {
+		match parse_expression(&tokens[start..]) {
+			Some((instruction, tokens_consumed)) => {
+				instructions.push(instruction);
+				start = start + tokens_consumed;
+			},
+			None => break
 		}
 	}
+
 	instructions
 }
 
 #[test]
 fn parse_input_test() {
 	let test_cases = vec!(
-		("123 -> x", vec!(Instruction::Load('x', 123))),
-		("456 -> y", vec!(Instruction::Load('y', 456))),
-		("x AND y -> d", vec!(Instruction::And('d', 'x', 'y')))
+		("123 -> x", vec!(Instruction::Load("x", 123))),
+		("123 -> x\n456 -> y", vec!(Instruction::Load("x", 123), Instruction::Load("y", 456))),
+		("x AND y -> d", vec!(Instruction::And("d", "x", "y"))),
+		("x OR y -> e", vec!(Instruction::Or("e", "x", "y"))),
+		("x LSHIFT 2 -> f", vec!(Instruction::LShift("f", "x", 2))),
+		("y RSHIFT 2 -> g", vec!(Instruction::RShift("g", "y", 2))),
+		("NOT x -> h", vec!(Instruction::Not("h", "x"))),
+		("NOT y -> i", vec!(Instruction::Not("i", "y"))),
+		(
+			r"123 -> x
+			456 -> y
+			x AND y -> d
+			x OR y -> e
+			x LSHIFT 2 -> f
+			y RSHIFT 2 -> g
+			NOT x -> h
+			NOT y -> i", 
+			vec!(
+				Instruction::Load("x", 123),
+				Instruction::Load("y", 456),
+				Instruction::And("d", "x", "y"),
+				Instruction::Or("e", "x", "y"),
+				Instruction::LShift("f", "x", 2),
+				Instruction::RShift("g", "y", 2),
+				Instruction::Not("h", "x"),
+				Instruction::Not("i", "y"),
+			)
+		)
 	);
 
 	for (i, test_case) in test_cases.iter().enumerate() {
 		let (input, ref expect) = *test_case;
 
-		println!("Test case #{:?}: input = {:?} expect = {:?}", 
-			i, input, expect);
-
+		println!("Test case #{:?}: input = {:?} expect = {:?}", i, input, expect);
 		assert_eq!(*expect, parse_input(input));
 	}
 }
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum Token {
-	And,
-	Assign,
-	Constant(u32),
-	LShift,
-	Not,
-	Or,
-	RShift,
-	Wire(char)
+fn parse_expression<'a>(tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Constant(c) => match parse_constant_assign(c, &tokens[1..]) {
+			Some((instruction, tokens_consumed)) => {
+				Some((instruction, tokens_consumed + 1))
+			},
+			None => None
+		},
+		Token::Wire(w) => match tokens[1] {
+			Token::And => match parse_and(w, &tokens[2..]) {
+				Some((instruction, tokens_consumed)) => {
+					Some((instruction, tokens_consumed + 2))
+				},
+				None => None,
+			},
+			Token::Or => match parse_or(w, &tokens[2..]) {
+				Some((instruction, tokens_consumed)) => {
+					Some((instruction, tokens_consumed + 2))
+				},
+				None => None,
+			},
+			Token::LShift => match parse_lshift(w, &tokens[2..]) {
+				Some((instruction, tokens_consumed)) => {
+					Some((instruction, tokens_consumed + 2))
+				},
+				None => None,
+			},
+			Token::RShift => match parse_rshift(w, &tokens[2..]) {
+				Some((instruction, tokens_consumed)) => {
+					Some((instruction, tokens_consumed + 2))
+				},
+				None => None,
+			},
+			_ => None
+		},
+		Token::Not => match parse_not(&tokens[1..]){
+			Some((instruction, tokens_consumed)) => {
+				Some((instruction, tokens_consumed + 1))
+			}, 
+			None => None
+		},
+		_ => None
+	}
 }
 
+fn parse_constant_assign<'a>(c: u16, tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Assign => match tokens[1] {
+			Token::Wire(w) => {
+				Some((Instruction::Load(w, c), 2))
+			},
+			_ => None,
+		},
+		_ => None
+	}
+}
+
+fn parse_and<'a>(w1: &'a str, tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Wire(w2) => match tokens[1] {
+			Token::Assign => match tokens[2] {
+				Token::Wire(d) => Some((Instruction::And(d, w1, w2), 3)),
+				_ => None,
+			},
+			_ => None,
+		},
+		_ => None,
+	}
+}
+
+fn parse_or<'a>(w1: &'a str, tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Wire(w2) => match tokens[1] {
+			Token::Assign => match tokens[2] {
+				Token::Wire(d) => Some((Instruction::Or(d, w1, w2), 3)),
+				_ => None,
+			},
+			_ => None,
+		},
+		_ => None,
+	}
+}
+
+fn parse_lshift<'a>(w1: &'a str, tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Constant(c) => match tokens[1] {
+			Token::Assign => match tokens[2] {
+				Token::Wire(d) => Some((Instruction::LShift(d, w1, c), 3)),
+				_ => None,
+			},
+			_ => None,
+		},
+		_ => None,
+	}
+}
+
+fn parse_rshift<'a>(w1: &'a str, tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Constant(c) => match tokens[1] {
+			Token::Assign => match tokens[2] {
+				Token::Wire(d) => Some((Instruction::RShift(d, w1, c), 3)),
+				_ => None,
+			},
+			_ => None,
+		},
+		_ => None,
+	}
+}
+
+fn parse_not<'a>(tokens: &[Token<'a>]) -> Option<(Instruction<'a>, usize)> {
+	match tokens[0] {
+		Token::Wire(w) => match tokens[1] {
+			Token::Assign => match tokens[2] {
+				Token::Wire(d) => Some((Instruction::Not(d, w), 3)),
+				_ => None,
+			},
+			_ => None,
+		},
+		_ => None,
+	}
+}
+
+#[test]
+fn parse_expression_test() {
+	let input = vec!(Token::Constant(123), Token::Assign, Token::Wire("x"));
+	let expect = Instruction::Load("x", 123);
+	let (actual, tokens_consumed) = parse_expression(&input[..]).unwrap();
+	assert_eq!(expect, actual);
+	assert_eq!(3, tokens_consumed);
+
+	let input = vec!(
+		Token::Constant(123), Token::Assign, Token::Wire("x"),
+		Token::Constant(456), Token::Assign, Token::Wire("y"),
+	);
+	let expect = Instruction::Load("x", 123);
+	let start = 0;
+	let (actual, tokens_consumed) = parse_expression(&input[start..]).unwrap();
+	assert_eq!(expect, actual);
+	assert_eq!(3, tokens_consumed);
+
+	let expect = Instruction::Load("y", 456);
+	let (actual, tokens_consumed) = parse_expression(&input[start+tokens_consumed..]).unwrap();
+	assert_eq!(expect, actual);
+	assert_eq!(3, tokens_consumed);
+}
+                                                                        
 fn tokenize_input(input: &str) -> Vec<Token> {
-	use std::str::FromStr;
-
-	let mut tokens: Vec<Token> = Vec::new();
-
-	let sub_strings: Vec<&str> = input.split_whitespace().collect();
-
-	for sub_string in sub_strings.clone() {
-		if sub_string.chars().all(|c| c.is_digit(10)) {
-			// We've got a constant so let's create the token 
-			// for it 
-			let value = u32::from_str(sub_string).unwrap();
-			tokens.push(Token::Constant(value));
-		} else if sub_string == "->" {
-			tokens.push(Token::Assign);
-		} else if sub_string == "AND" {
-			tokens.push(Token::And);
-		} else if sub_string == "OR" {
-			tokens.push(Token::Or);
-		} else if sub_string == "LSHIFT" {
-			tokens.push(Token::LShift);
-		} else if sub_string == "RSHIFT" {
-			tokens.push(Token::RShift);
-		} else if sub_string == "NOT" {
-			tokens.push(Token::Not);
-		} else if sub_string.len() == 1 {
-			let c = sub_string.chars().nth(0).unwrap();
-			tokens.push(Token::Wire(c));
+	use std::str::FromStr;											   
+                                                                       
+	let mut tokens: Vec<Token> = Vec::new();						   
+	                                                                   
+	let sub_strings: Vec<&str> = input.split_whitespace().collect();   
+                                                                       
+	for sub_string in sub_strings {                                    
+		match sub_string {
+			"->" => {
+				tokens.push(Token::Assign);
+			},
+			"AND" => {
+				tokens.push(Token::And);
+			},
+			"OR" => {
+				tokens.push(Token::Or);
+			},
+			"LSHIFT" => {
+				tokens.push(Token::LShift);
+			},
+			"RSHIFT" => {
+				tokens.push(Token::RShift);
+			},
+			"NOT" => {
+				tokens.push(Token::Not);
+			},
+			s => if s.chars().all(|c| c.is_digit(10)) {
+				let constant = u16::from_str(s).unwrap();
+				tokens.push(Token::Constant(constant));
+			} else {
+				let wire_name: &str = s.clone();
+				tokens.push(Token::Wire(wire_name));
+			},
 		}
 	}
 
@@ -104,14 +276,14 @@ fn tokenize_input(input: &str) -> Vec<Token> {
 #[test]
 fn tokenize_input_test() {
 	let test_cases = vec!(
-		("123 -> x", vec!(Token::Constant(123), Token::Assign, Token::Wire('x'))),
-		("456  -> y", vec!(Token::Constant(456), Token::Assign, Token::Wire('y'))),
-		("x AND y -> d", vec!(Token::Wire('x'), Token::And, Token::Wire('y'), Token::Assign, Token::Wire('d'))),
-		("x OR y -> e", vec!(Token::Wire('x'), Token::Or, Token::Wire('y'), Token::Assign, Token::Wire('e'))),
-		("x LSHIFT 2 -> f", vec!(Token::Wire('x'), Token::LShift, Token::Constant(2), Token::Assign, Token::Wire('f'))),
-		("y RSHIFT 2 -> g", vec!(Token::Wire('y'), Token::RShift, Token::Constant(2), Token::Assign, Token::Wire('g'))),
-		("NOT x -> h", vec!(Token::Not, Token::Wire('x'), Token::Assign, Token::Wire('h'))),
-		("NOT y -> i", vec!(Token::Not, Token::Wire('y'), Token::Assign, Token::Wire('i'))),
+		("123 -> x", vec!(Token::Constant(123), Token::Assign, Token::Wire("x"))),
+		("456  -> y", vec!(Token::Constant(456), Token::Assign, Token::Wire("y"))),
+		("x AND y -> d", vec!(Token::Wire("x"), Token::And, Token::Wire("y"), Token::Assign, Token::Wire("d"))),
+		("x OR y -> e", vec!(Token::Wire("x"), Token::Or, Token::Wire("y"), Token::Assign, Token::Wire("e"))),
+		("x LSHIFT 2 -> f", vec!(Token::Wire("x"), Token::LShift, Token::Constant(2), Token::Assign, Token::Wire("f"))),
+		("y RSHIFT 2 -> g", vec!(Token::Wire("y"), Token::RShift, Token::Constant(2), Token::Assign, Token::Wire("g"))),
+		("NOT x -> h", vec!(Token::Not, Token::Wire("x"), Token::Assign, Token::Wire("h"))),
+		("NOT y -> i", vec!(Token::Not, Token::Wire("y"), Token::Assign, Token::Wire("i"))),
 	);
 
 	for (i, test_case) in test_cases.iter().enumerate() {
