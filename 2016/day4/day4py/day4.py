fbea0e6c465ff2dad4fe6832ff7ced8692ebde26
Sean Kell <sean.michael.kell@gmail.com>
2016-12-04 12:01:10 -0500

    Implement part 2 of day 4

--- 2016/day4/day4py/day4.py
+++ 2016/day4/day4py/day4.py
@@ -2,9 +2,9 @@
 """
 
 import sys
-import operator
 
 def parse_room(line):
+    """Parses the components of the room entry from a line of input."""
     components = line.split('[')
     name_sector = components[0]
     checksum = components[1]
@@ -14,26 +14,45 @@ def parse_room(line):
     name = name_sector[0:last_dash]
     sector = name_sector[last_dash+1:]
 
-    return (name, sector, checksum)
+    return (name, int(sector), checksum)
 
 def check_room(room):
+    """Verifies the checksum of the given room."""
     name, _, checksum = room
 
     chars = {}
-    for char in name:
-        if char != '-':
-            if char not in chars:
-                chars[char] = 1
-            else:
-                chars[char] = chars[char] + 1
+    for char in name.translate(None, '-'):
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] = chars[char] + 1
 
     sorted_dict = sorted(chars.items(), key=lambda x: (-x[1], x[0]))
     new_checksum = ''
     for char, _ in sorted_dict[:5]:
         new_checksum = new_checksum + char
 
-    print '{0} == {1} = {2}'.format(checksum, new_checksum, checksum == new_checksum)
     return checksum == new_checksum
+
+def shift_char(char, shift):
+    """Shifts the given character to the left alphabetically `shift` times."""
+    new_char = chr((((ord(char) - 97) + shift) % 26) + 97)
+    return new_char
+
+
+def decrypt_name(room):
+    """Descrypts the name of a room (assumes the room is valid)."""
+    name, sector, _ = room
+
+    decoded = ''
+    for char in name:
+        if char != '-':
+            decoded = decoded + shift_char(char, sector)
+        else:
+            decoded = decoded + ' '
+
+    return decoded
+
 
 def part1(lines):
     """Process the lines according to the requirements for part 1."""
@@ -54,6 +73,13 @@ def part2(lines):
     """Process the lines according to the requirements for part 2."""
     print 'Part 2'
     print '------'
+
+    rooms = [room for room in [parse_room(line) for line in lines] if check_room(room)]
+    for room in rooms:
+        decoded = decrypt_name(room)
+        if 'northpole object storage' in decoded:
+            print 'decoded - sector {0}'.format(room[1])
+
 
 def main():
     """Main entry point."""
